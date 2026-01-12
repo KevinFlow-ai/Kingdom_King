22b93c330930347bbafa13bca8dce4188b4ee8c6
KevinFlow-ai <cosoyeray@gmail.com>
2026-01-12 20:02:28 +0100

    Initial commit

---
+++ app/src/main/java/com/example/kingdom_king/PagA_Splash_Screen_Activity.java
@@ -0,0 +1,43 @@
+package com.example.kingdom_king;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PagA_Splash_Screen_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_pag1_splash_screen);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+
+        Button Login_btn = findViewById(R.id.buttonsplash);
+
+
+
+
+        Login_btn.setOnClickListener(V -> {
+            // Acciones a realizar cuando se hace clic en el botón
+
+            Intent intent = new Intent(PagA_Splash_Screen_Activity.this, PagB_Login_Activity.class);
+            startActivity(intent);
+        });
+
+
+    }
+}
