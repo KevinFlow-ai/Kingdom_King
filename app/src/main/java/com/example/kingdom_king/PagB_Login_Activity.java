41fe2a893eb7b7c447f79817ac124d98773dd533
KevinFlow-ai <cosoyeray@gmail.com>
2026-01-13 13:16:16 +0100

    Implementación de login finalizado.
    - Se agregaron los botones de Apple y Google en `PagB_Login_Activity` con redirección a sus respectivos sitios web.
    - Se actualizó el layout `activity_pag2_login.xml` incluyendo nuevos elementos de UI (CheckBox, TextView para recuperar contraseña y botones de login).
    - Se añadieron recursos gráficos (drawables) para los nuevos botones e iconos.

--- app/src/main/java/com/example/kingdom_king/PagB_Login_Activity.java
+++ app/src/main/java/com/example/kingdom_king/PagB_Login_Activity.java
@@ -1,9 +1,12 @@
 package com.example.kingdom_king;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,10 +18,32 @@ public class PagB_Login_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_pag2_login);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+        // Botones de inicio de sesión con Apple y Google
+        AppCompatButton btnApple = findViewById(R.id.button_loggin_apple); // identificación del botón apple
+        AppCompatButton btnGoogle = findViewById(R.id.button_loggin_google); // identificación del botón google
+
+
+        btnApple.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://appleid.apple.com/"));
+            startActivity(intent);
+        });
+
+        btnGoogle.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://accounts.google.com/"));
+            startActivity(intent);
+        });
+
+
     }
 }
