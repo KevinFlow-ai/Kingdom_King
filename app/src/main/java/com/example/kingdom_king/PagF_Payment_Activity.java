d730c00f101c190b196499b783be16f585e6744c
KevinFlow-ai <cosoyeray@gmail.com>
2026-02-03 17:29:46 +0100

    Implementación de navegación y comandos de voz en la pantalla de pago. F->C(`PagF_Payment_Activity`).
    - Se implementó la lógica para el botón "continuar comprando" mediante un `ConstraintLayout` que redirige a `PagC_Home_Activity`.
    - Se añadió el método `onVoiceCommand` para procesar el comando "continuar pagando", el cual ejecuta una respuesta de voz y navega hacia la pantalla principal.

--- app/src/main/java/com/example/kingdom_king/PagF_Payment_Activity.java
+++ app/src/main/java/com/example/kingdom_king/PagF_Payment_Activity.java
@@ -1,14 +1,13 @@
 package com.example.kingdom_king;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class PagF_Payment_Activity extends AppCompatActivity {
+public class PagF_Payment_Activity extends BaseVoiceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +24,34 @@ public class PagF_Payment_Activity extends AppCompatActivity {
 
             return insets;
         });
+
+
+        // boton manual para cuando pulse el contenedor de seguir comprando pase al home
+        ConstraintLayout btnContinuarComprando = findViewById(R.id.btn_continuar_comprando);
+        btnContinuarComprando.setOnClickListener(v -> irAPaginaHome());
+
     }
+
+
+
+    private void irAPaginaHome() {
+        Intent intent = new Intent(this, PagC_Home_Activity.class);
+        startActivity(intent);
+    }
+
+
+    @Override
+    protected void onVoiceCommand(String comando) {
+        String comandoNormalizado = comando.toLowerCase().trim();
+
+        // Si el usuario dice "comprar"
+        if (comandoNormalizado.contains("continuar pagando")) {
+            hablar("compra realizada, volviendo a la pagina principal");
+            irAPaginaHome();
+
+        }
+
+
+    }
+
 }
