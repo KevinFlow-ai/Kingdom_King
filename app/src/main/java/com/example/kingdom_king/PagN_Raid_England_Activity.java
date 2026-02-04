27ac20f40377c1912c698251d502dd329ab12b28
KevinFlow-ai <cosoyeray@gmail.com>
2026-02-04 03:51:34 +0100

    Implementación de la interfaz de "Raid England" (Página 14).
    - Se creó la clase `PagN_Raid_England_Activity.java` con soporte para `EdgeToEdge` y configuración básica de insets.
    - Se añadió el layout `activity_pag14_raid_england.xml` basado en un `ConstraintLayout`.
    - Se registró la nueva actividad `PagN_Raid_England_Activity` en el `AndroidManifest.xml`.

---
+++ app/src/main/java/com/example/kingdom_king/PagN_Raid_England_Activity.java
@@ -0,0 +1,27 @@
+package com.example.kingdom_king;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PagN_Raid_England_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_pag14_raid_england);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            /*
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+             */
+            return insets;
+        });
+    }
+}
