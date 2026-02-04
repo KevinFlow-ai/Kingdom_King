4716dfc83d0dffe85252f1d3c2eaef60e0a1711f
KevinFlow-ai <cosoyeray@gmail.com>
2026-02-04 16:48:13 +0100

    Implementación de navegación manual de retorno en la pantalla de Raid England. N->M.
    - Se añadió un listener al componente `btn_volver_atras` en `PagN_Raid_England_Activity` para permitir la navegación manual hacia `PagM_Raid_Incursiones_Activity`.
    - Se comentaron los ajustes automáticos de `systemBars` insets para mantener la consistencia con el diseño personalizado.

--- app/src/main/java/com/example/kingdom_king/PagN_Raid_England_Activity.java
+++ app/src/main/java/com/example/kingdom_king/PagN_Raid_England_Activity.java
@@ -1,6 +1,8 @@
 package com.example.kingdom_king;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +18,7 @@ public class PagN_Raid_England_Activity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_pag14_raid_england);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
             /*
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -23,5 +26,14 @@ public class PagN_Raid_England_Activity extends AppCompatActivity {
              */
             return insets;
         });
+
+
+        // Botón manual: PARA VOLVER ATRÁS (Vuelve a PagC)
+        ImageView btnFlechaAtras = findViewById(R.id.btn_volver_atras);
+        btnFlechaAtras.setOnClickListener(v -> {
+            Intent intent = new Intent(this, PagM_Raid_Incursiones_Activity.class);
+            startActivity(intent);
+        });
+
     }
 }
